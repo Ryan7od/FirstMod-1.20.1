10f75868f2a0c6e3723d57df482ab69597acce98
RyanOD <ryan7od@hotmail.com>
2024-05-22 17:58:57 +0100

    First items added (tungsten and tungsten ore)

---
+++ src/main/java/net/ryanod/firstmod/item/ModCreativeModeTabs.java
@@ -0,0 +1,29 @@
+package net.ryanod.firstmod.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.ryanod.firstmod.FirstMod;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, FirstMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> MATERIALS_TAB = CREATIVE_MODE_TABS.register("materials_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.TUNGSTEN.get()))
+                    .title(Component.translatable("creativetab.materials_tab"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModItems.TUNGSTEN.get());
+                        pOutput.accept(ModItems.TUNGSTEN_ORE.get());
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
---
+++ src/main/java/net/ryanod/firstmod/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.ryanod.firstmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.ryanod.firstmod.FirstMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, FirstMod.MOD_ID);
+
+    public static final RegistryObject<Item> TUNGSTEN = ITEMS.register("tungsten",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> TUNGSTEN_ORE = ITEMS.register("tungsten_ore",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
